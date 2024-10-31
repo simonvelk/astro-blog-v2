9131a52515b5a259d0dd0e953d4824bf1de63c7b
Simon Velk <simon.velk@gmail.com>
2024-10-31 19:33:20 +1300

    Finished!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+import preact from "@astrojs/preact";
+
+export default defineConfig({
+  site: "http://localhost:4322/",
+  integrations: [preact()]
+});
